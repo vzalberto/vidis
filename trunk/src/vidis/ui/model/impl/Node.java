a0f64d7adeab8db421ef9e1dfe4d8829bb81ba09
admin@lab-9.com <admin@lab-9.com@9cfe6e55-b949-0410-9339-5d950d6e6b25>
2008-09-10 09:30:47 +0000

    fixed link label

--- trunk/src/vidis/ui/model/impl/Node.java
+++ trunk/src/vidis/ui/model/impl/Node.java
@@ -3,6 +3,7 @@ package vidis.ui.model.impl;
 import java.awt.Font;
 
 import javax.media.opengl.GL;
+import javax.vecmath.Vector3d;
 
 import vidis.data.var.AVariable;
 import vidis.data.var.IVariableContainer;
@@ -21,17 +22,17 @@ public class Node extends ASimObject {
 	
 	private static TextRenderer textRenderer = new TextRenderer( new Font("Times New Roman", Font.PLAIN, 130 ), true, true );
 
-	private void drawText(GL gl, String text, double angle, double x, double y, double z) {
+	private void drawText(GL gl, String text, double angle, double x, double y, double z, Vector3d move) {
 		gl.glPushMatrix();
-			gl.glTranslated(0.0, 0.8, 0.0);
+			gl.glCullFace(GL.GL_FRONT);
+			gl.glFrontFace(GL.GL_CW);
+			gl.glTranslated(0.0 + move.getX(), 0.8 + move.getY(), 0.0 + move.getZ());
 			gl.glScaled(0.001, 0.001, 0.001);
 			gl.glRotated(angle, x, y, z);
 			textRenderer.begin3DRendering();
 			textRenderer.setUseVertexArrays(false);
 			textRenderer.draw3D( text, 0f, 0f, 0f, 1f );
 			textRenderer.end3DRendering();
-			gl.glCullFace(GL.GL_FRONT);
-			gl.glFrontFace(GL.GL_CW);
 		gl.glPopMatrix();
 	}
 	
@@ -49,9 +50,23 @@ public class Node extends ASimObject {
 			// may happen, but if, don't care
 			text = getVariableById(AVariable.COMMON_IDENTIFIERS.ID).getData().toString();
 		} finally {
-			drawText(gl, text, 0, 0, 1, 0);
-			drawText(gl, text, 120, 0, 1, 0);
-			drawText(gl, text, 240, 0, 1, 0);
+			//Rectangle2D bounds = textRenderer.getBounds(text);
+			//double width = bounds.getWidth();
+			//double scale = (width/2) / 350;
+			// front
+			gl.glPushMatrix();
+				// rotate the whole thingy by 180
+				gl.glRotated(180, 0, 1, 0);
+				drawText(gl, text, 0, 0, 1, 0, new Vector3d(0, 0, 0));
+//				// front
+//				drawText(gl, text, 0, 0, 1, 0, new Vector3d(scale,0,scale*1));
+//				// right
+//				drawText(gl, text, 90, 0, 1, 0, new Vector3d(scale*-1,0,-scale));
+//				// back
+//				drawText(gl, text, 180, 0, 1, 0, new Vector3d(-scale,0,scale*-1));
+//				// left
+//				drawText(gl, text, 270, 0, 1, 0, new Vector3d(scale*1,0,scale));
+			gl.glPopMatrix();
 		}
 		// draw node
 		gl.glColor3d( 1, 0, 0 );
@@ -66,8 +81,7 @@ public class Node extends ASimObject {
 
 	@Override
 	protected void handleEvent(IVidisEvent e) {
-		// TODO Auto-generated method stub
-		
+		System.err.println(e);
 	}
 
 }
