a9de88cece3c7078a3d24642834e7124866171f8
admin@lab-9.com <admin@lab-9.com@9cfe6e55-b949-0410-9339-5d950d6e6b25>
2008-09-10 18:25:21 +0000

    created packet and node gui elements for the right gui container

---
+++ trunk/src/vidis/ui/model/impl/guielements/PacketGuiElement.java
@@ -0,0 +1,29 @@
+package vidis.ui.model.impl.guielements;
+
+import javax.media.opengl.GL;
+
+import vidis.ui.model.impl.Packet;
+import vidis.ui.model.structure.AGuiContainer;
+
+public class PacketGuiElement extends AGuiContainer {
+	//private static Logger logger = Logger.getLogger(PacketGuiElement.class);
+	
+	private Packet packet;
+	
+	public PacketGuiElement(Packet packet) {
+		this.packet = packet;
+	}
+	
+	private Packet getPacket() {
+		return packet;
+	}
+	
+	@Override
+	public void renderContainer(GL gl) {
+		// draw heading
+		// FIXME
+		// draw content
+		// FIXME
+	}
+
+}
