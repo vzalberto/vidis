e66962a1f37574f5fb1dfc261a08254a1bf52731
admin <admin@lab-9.com>
2008-09-13 12:35:59 +0000

    some buttons (very basic and stupid - don't work - but is a nice base)

---
+++ src/vidis/ui/model/impl/guielements/buttons/ButtonPlay.java
@@ -0,0 +1,12 @@
+package vidis.ui.model.impl.guielements.buttons;
+
+import org.apache.log4j.Logger;
+
+import vidis.ui.model.impl.guielements.Button;
+
+public class ButtonPlay extends Button {
+	private static Logger logger = Logger.getLogger(ButtonPlay.class);
+	public ButtonPlay() {
+		super("Play");
+	}
+}
