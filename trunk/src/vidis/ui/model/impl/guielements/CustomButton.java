fbe7e861e3f96390ff7a726dd719c25042fb7da5
admin@lab-9.com <admin@lab-9.com@9cfe6e55-b949-0410-9339-5d950d6e6b25>
2008-09-22 18:22:22 +0000

    minor changes

---
+++ trunk/src/vidis/ui/model/impl/guielements/CustomButton.java
@@ -0,0 +1,15 @@
+package vidis.ui.model.impl.guielements;
+
+import org.apache.log4j.Logger;
+
+import vidis.ui.model.impl.BasicGuiContainer;
+
+/**
+ * this button is a button without a label
+ * @author Dominik
+ *
+ */
+public class CustomButton extends BasicGuiContainer {
+	private static Logger logger = Logger.getLogger(CustomButton.class);
+
+}
