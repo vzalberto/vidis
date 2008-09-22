c42d9d3c6cc5f8d763ca7812b970c50d4889fa41
admin <admin@lab-9.com>
2008-09-22 18:27:47 +0000

    some buttons

---
+++ src/vidis/ui/model/impl/guielements/buttons/CloseButton.java
@@ -0,0 +1,10 @@
+package vidis.ui.model.impl.guielements.buttons;
+
+import org.apache.log4j.Logger;
+
+import vidis.ui.model.impl.guielements.CustomButton;
+
+public class CloseButton extends CustomButton {
+	private static Logger logger = Logger.getLogger(CloseButton.class);
+
+}
