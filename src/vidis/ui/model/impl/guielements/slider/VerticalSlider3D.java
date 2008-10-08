3e5a71efd72f1b0ebeaea92de7ec68c1e24a4046
admin <admin@lab-9.com>
2008-10-08 17:09:44 +0000

    sliders yoyo

--- src/vidis/ui/model/impl/guielements/slider/VerticalSlider3D.java
+++ src/vidis/ui/model/impl/guielements/slider/VerticalSlider3D.java
@@ -1,7 +1,7 @@
 package vidis.ui.model.impl.guielements.slider;
 
 public class VerticalSlider3D extends ASlider3D {
-	public VerticalSlider3D() {
-		super(ASlider3D.VERTICAL);
+	public VerticalSlider3D(int i, int j) {
+		super(i, j);
 	}
 }
