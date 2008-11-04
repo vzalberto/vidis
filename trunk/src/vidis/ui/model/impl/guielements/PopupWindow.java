a720ccfd684fc31c74aa0816d415cf8645332080
admin@lab-9.com <admin@lab-9.com@9cfe6e55-b949-0410-9339-5d950d6e6b25>
2008-11-04 19:09:09 +0000

    added apply layout popup
    fixed bug with load msim popup

--- trunk/src/vidis/ui/model/impl/guielements/PopupWindow.java
+++ trunk/src/vidis/ui/model/impl/guielements/PopupWindow.java
@@ -5,14 +5,27 @@ import java.awt.Color;
 import org.apache.log4j.Logger;
 
 import vidis.ui.model.impl.BasicGuiContainer;
+import vidis.ui.model.impl.Label;
 import vidis.ui.model.impl.PercentMarginLayout;
+import vidis.ui.model.impl.guielements.popupWindows.OpenMSIMFilePopupWindow;
+import vidis.ui.model.structure.IGuiContainer;
 
 public abstract class PopupWindow extends BasicGuiContainer {
 	private static Logger logger = Logger.getLogger(PopupWindow.class);
 	
-	public PopupWindow() {
+	public PopupWindow(String title) {
 		color1 = new Color(150, 150, 150, 150);
 		color2 = new Color(150, 150, 150, 0);
 		this.setLayout( new PercentMarginLayout(-0.05, -0.5, -0.35, -0.15, -0.35, -0.6));
+		
+		// titlebar
+		Label tmp = new Label(title);
+		tmp.setLayout( new PercentMarginLayout(0, -0.93, 0, 0, -0.07, -1) );
+		tmp.setTextColor( Color.LIGHT_GRAY );
+		addChild( tmp );
+	}
+	
+	protected final void close() {
+		getParent().removeChild( this );
 	}
 }
