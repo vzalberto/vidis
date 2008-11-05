97b00f931032e354ca1385aeed7452a2198700ed
admin@lab-9.com <admin@lab-9.com@9cfe6e55-b949-0410-9339-5d950d6e6b25>
2008-11-05 15:01:40 +0000

    added gui tree

---
+++ trunk/src/vidis/ui/model/impl/guielements/popupWindows/OpenMSIMFilePopupWindow_tree.java
@@ -0,0 +1,60 @@
+package vidis.ui.model.impl.guielements.popupWindows;
+
+import java.io.File;
+
+import org.apache.log4j.Logger;
+
+import vidis.ui.events.IVidisEvent;
+import vidis.ui.events.VidisEvent;
+import vidis.ui.model.impl.PercentMarginLayout;
+import vidis.ui.model.impl.guielements.PopupWindow;
+import vidis.ui.model.impl.guielements.tree.Tree;
+import vidis.ui.model.impl.guielements.tree.TreeElement;
+import vidis.ui.mvc.api.Dispatcher;
+import vidis.util.ResourceManager;
+
+public class OpenMSIMFilePopupWindow_tree extends PopupWindow {
+	private static Logger logger = Logger.getLogger(OpenMSIMFilePopupWindow_tree.class);
+	private static OpenMSIMFilePopupWindow_tree instance;
+	public static OpenMSIMFilePopupWindow_tree getInstance() {
+		if( instance == null)
+			instance = new OpenMSIMFilePopupWindow_tree("Please pick a module You want to load");
+		return instance;
+	}
+	public OpenMSIMFilePopupWindow_tree(String title) {
+		super(title);
+		Tree<File> tree = new Tree<File>("Modules", null) {
+			@Override
+			protected void clickedOn(TreeElement<File> element) {
+				logger.info("CLICKED ON: " + element.getObject().getName() );
+				if(element.getObject().isDirectory()) {
+					// module directory, expand/close
+					if(element.isExpanded())
+						element.collapse();
+					else
+						element.expand();
+					// refresh display
+					refresh();
+				} else {
+					// module file, open
+					Dispatcher.forwardEvent(new VidisEvent<File>(IVidisEvent.SimulatorLoad, element.getObject()));
+					// close this popup
+					close();
+				}
+			}
+		};
+		for( File module : ResourceManager.getModules() ) {
+			TreeElement<File> treeModule = tree.createTreeElement(module.getName(), module);
+			tree.addChild( treeModule );
+			for( File moduleFile : ResourceManager.getModuleFiles(module)) {
+				treeModule.addChild(
+						tree.createTreeElement(moduleFile.getName(), moduleFile)
+				);
+			}
+		}
+		tree.refresh();
+		tree.setLayout( new PercentMarginLayout(0,0,0,0,-1,-1));
+		tree.getScrollPane().setLayout(new PercentMarginLayout(0,0,0,-0.07,-0.93,-1));
+		this.addChild( tree.getScrollPane() );
+	}
+}
---
+++ trunk/src/vidis/ui/model/impl/guielements/tree/Tree.java
@@ -0,0 +1,29 @@
+package vidis.ui.model.impl.guielements.tree;
+
+import org.apache.log4j.Logger;
+
+import vidis.ui.model.impl.guielements.scrollpane.ScrollPane3D;
+import vidis.ui.model.structure.AGuiContainer;
+
+public abstract class Tree<T> extends TreeElement<T> {
+	private static Logger logger = Logger.getLogger(Tree.class);
+	public Tree(String name, T obj) {
+		super(name, obj);
+		root = this;
+		scrollpane = new ScrollPane3D();
+		expand();
+	}
+	private ScrollPane3D scrollpane;
+	
+	public TreeElement<T> createTreeElement(String name, T obj) {
+		return new TreeElement<T>(this, name, obj);
+	}
+	public AGuiContainer getScrollPane() {
+		return scrollpane;
+	}
+	public void refresh() {
+		scrollpane.removeAllChilds();
+		super.refresh(0);
+	}
+	protected abstract void clickedOn(TreeElement<T> element);
+}
