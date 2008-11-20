3f83ddeba95f005990d9d145102038e7c47a5ddd
admin <admin@lab-9.com>
2008-11-20 13:47:51 +0000

    refactor

--- src/vidis/ui/events/MouseClickedEvent.java
+++ src/vidis/ui/events/MouseClickedEvent.java
@@ -2,10 +2,6 @@ package vidis.ui.events;
 
 import java.awt.event.MouseEvent;
 
-import javax.vecmath.Point2d;
-import javax.vecmath.Point4d;
-import javax.vecmath.Vector4d;
-
 public class MouseClickedEvent extends AMouseEvent {
 
 	public MouseClickedEvent(MouseEvent m) {
