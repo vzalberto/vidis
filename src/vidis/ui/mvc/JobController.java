1422d13fc890d1876a300cc32041bdc6144b9cf4
admin <admin@lab-9.com>
2008-12-09 15:47:38 +0000

    job controller++
    cross dependencies

--- src/vidis/ui/mvc/JobController.java
+++ src/vidis/ui/mvc/JobController.java
@@ -1,7 +1,13 @@
 package vidis.ui.mvc;
 
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Future;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
@@ -9,6 +15,7 @@ import org.apache.log4j.Logger;
 
 import vidis.ui.events.IVidisEvent;
 import vidis.ui.events.JobAppend;
+import vidis.ui.events.jobs.IJob;
 import vidis.ui.mvc.api.AController;
 
 /**
@@ -26,14 +33,47 @@ public class JobController extends AController {
 		registerEvent( IVidisEvent.AppendJob );
 	}
 	
+	private Map<IJob, Future<?>> futureJobs = new HashMap<IJob, Future<?>>();
+	private List<IJob> futureJobsToRemove = new LinkedList<IJob>();
+	
 	@Override
 	public void handleEvent(IVidisEvent event) {
 		switch ( event.getID() ) {
 			case IVidisEvent.AppendJob:
-				if ( event instanceof JobAppend )
+				if ( event instanceof JobAppend ) {
 //					jobs.offer(((JobAppend)event).getJob());
-					executer.execute( ((JobAppend)event).getJob() );
-				else
+					IJob j = ((JobAppend)event).getJob();
+					boolean submit = true;
+					if( j.mustExecuteUniquely() ) {
+						// check futures
+						for(Entry<IJob, Future<?>> e : futureJobs.entrySet()) {
+							if(e.getValue().isDone()) {
+								// clean done jobs
+								futureJobsToRemove.add(e.getKey());
+							} else {
+								if(e.getKey().getClass().equals(j.getClass())) {
+									// cancel old job
+									if(e.getValue().cancel(true)) {
+										// job cancelled, fine
+									} else {
+										// could not be cancelled, do not submit new job
+										submit = false;
+									}
+								}
+							}
+						}
+						// remove unused futures
+						for(IJob tmp : futureJobsToRemove)
+							futureJobs.remove(tmp);
+						futureJobsToRemove.clear();
+					}
+					if(submit) {
+						Future<?> fj = executer.submit(j);
+						futureJobs.put(j, fj);
+					}
+//					// just execute it
+//					executer.execute( j );
+				} else
 					logger.warn("received a job that I cannot handle; claims to be append job event but the class is not compatible!");
 				break;
 		}
