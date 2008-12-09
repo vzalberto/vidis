3f67cf6bb0c1b205a762f3f5d5b8676e14ecf9cf
admin@lab-9.com <admin@lab-9.com@9cfe6e55-b949-0410-9339-5d950d6e6b25>
2008-12-09 15:35:49 +0000

    jobcontroller++
    the job controller now can check jobs and execute only what he wants to execute :-P

--- trunk/src/vidis/ui/events/jobs/jobs/layouts/ALayoutJob.java
+++ trunk/src/vidis/ui/events/jobs/jobs/layouts/ALayoutJob.java
@@ -1,6 +1,8 @@
-package vidis.ui.events.jobs;
+package vidis.ui.events.jobs.jobs.layouts;
 
 import org.apache.log4j.Logger;
+
+import vidis.ui.events.jobs.ILayoutJob;
 
 public abstract class ALayoutJob implements ILayoutJob {
 	private static Logger logger = Logger.getLogger(ALayoutJob.class);
@@ -10,6 +12,9 @@ public abstract class ALayoutJob implements ILayoutJob {
 		} catch (Exception e) {
 			logger.error(e);
 		}
+	}
+	public boolean mustExecuteUniquely() {
+		return false;
 	}
 
 }
---
+++ trunk/src/vidis/ui/events/jobs/jobs/layouts/GraphElectricSpringLayoutJob.java
@@ -0,0 +1,9 @@
+package vidis.ui.events.jobs.jobs.layouts;
+
+
+public abstract class GraphElectricSpringLayoutJob extends ALayoutJob {
+	@Override
+	public boolean mustExecuteUniquely() {
+		return true;
+	}
+}
