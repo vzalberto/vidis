0272782928d234dbf91c7ff218cc3a5b49131d03
admin <admin@lab-9.com>
2008-12-13 13:20:24 +0000

    mst algorithm++
    working explore/echo!
    
    todo: ping/pong

--- src/vidis/modules/mstAlgorithm/EchoPacket.java
+++ src/vidis/modules/mstAlgorithm/EchoPacket.java
@@ -1,13 +1,20 @@
 package vidis.modules.mstAlgorithm;
 
+import java.util.Map;
+
 import org.apache.log4j.Logger;
 
 public class EchoPacket extends AMSTPacket {
 	private static Logger logger = Logger.getLogger(EchoPacket.class);
-	public EchoPacket(int id, String senderId) {
+	private Map<String, Long> distances;
+	public EchoPacket(int id, String senderId, Map<String, Long> distances) {
 		super(Type.ECHO, id, senderId);
+		this.distances = distances;
 	}
 	public EchoPacket(EchoPacket p) {
 		super(p);
+	}
+	public Map<String, Long> getDistances() {
+		return distances;
 	}
 }
