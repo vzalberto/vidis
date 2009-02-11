317d8dea51181c6d9818b22d71de6ea71125705c
admin@lab-9.com <admin@lab-9.com@9cfe6e55-b949-0410-9339-5d950d6e6b25>
2009-02-11 13:12:38 +0000

    huge update: - added copyright headers (except package vidis.util.graph) - minor refactoring

--- trunk/src/vidis/ui/model/graph/layouts/impl/GraphCenterLayout.java
+++ trunk/src/vidis/ui/model/graph/layouts/impl/GraphCenterLayout.java
@@ -1,3 +1,8 @@
+/*	VIDIS is a simulation and visualisation framework for distributed systems.
+	Copyright (C) 2009 Dominik Psenner, Christoph Caks
+	This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
+	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+	You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>. */
 package vidis.ui.model.graph.layouts.impl;
 
 import java.util.ArrayList;
@@ -11,7 +16,7 @@ import org.apache.log4j.Logger;
 import vidis.data.sim.SimNode;
 import vidis.data.var.vars.AVariable;
 import vidis.ui.model.graph.layouts.AGraphLayout;
-import vidis.ui.model.graph.layouts.GraphLayout;
+import vidis.ui.model.graph.layouts.IGraphLayout;
 
 /**
  * center layout for nodes
@@ -25,13 +30,13 @@ import vidis.ui.model.graph.layouts.GraphLayout;
 public class GraphCenterLayout extends AGraphLayout {
 	private static Logger logger = Logger.getLogger( GraphCenterLayout.class );
 	
-	private static GraphLayout instance = null;
+	private static IGraphLayout instance = null;
 	
 	private GraphCenterLayout() {
 		setNodeDensity(0.3);
 	}
 	
-	public static GraphLayout getInstance() {
+	public static IGraphLayout getInstance() {
 		if(instance == null)
 			instance = new GraphCenterLayout();
 		return instance;
