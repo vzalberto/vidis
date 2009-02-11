3d4973c5c3c2b0c1c4c35e5c528b2429cc0ba0c6
admin@lab-9.com <admin@lab-9.com@9cfe6e55-b949-0410-9339-5d950d6e6b25>
2009-02-11 16:38:21 +0000

    modified copyright header; it looks better like this

--- trunk/src/vidis/data/AUserLink.java
+++ trunk/src/vidis/data/AUserLink.java
@@ -1,8 +1,22 @@
-/*	VIDIS is a simulation and visualisation framework for distributed systems.
-	Copyright (C) 2009 Dominik Psenner, Christoph Caks
-	This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
-	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-	You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>. */
+/*
+ * VIDIS is a simulation and visualisation framework for distributed
+ * systems.
+ * 
+ * Copyright (C) 2009 Dominik Psenner, Christoph Caks
+ *  
+ * This program is free software; you can redistribute it and/or modify it 
+ * under the terms of the GNU General Public License as published by the 
+ * Free Software Foundation; either version 3 of the License, or (at your 
+ * option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful, but 
+ * WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU 
+ * General Public License for more details.
+ *  
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see <http://www.gnu.org/licenses/>.
+ */
 package vidis.data;
 
 import java.util.List;
@@ -20,94 +34,99 @@ import vidis.data.var.vars.AVariable.COMMON_SCOPES;
  * abstract user link represents a link by a user;
  * 
  * module writers should use this class to write their own link
+ * 
  * @author dominik
- *
+ * 
  */
 public abstract class AUserLink extends AUserComponent implements IUserLink {
-    //private ISimLinkCon simulatorComponent;
-    protected ISimLinkCon simulatorComponent;
+	// private ISimLinkCon simulatorComponent;
+	protected ISimLinkCon simulatorComponent;
 
-    public final void init(ISimLinkCon c) throws ObstructInitCallException {
+	public final void init(ISimLinkCon c) throws ObstructInitCallException {
 		if (simulatorComponent != null)
-		    throw new ObstructInitCallException();
+			throw new ObstructInitCallException();
 		this.simulatorComponent = c;
-    }
+	}
 
-    public final IUserNode getOtherNode(IUserNode me) {
+	public final IUserNode getOtherNode(IUserNode me) {
 		if (getNodeA().equals(me)) {
-		    return getNodeB();
-		} else if(getNodeB().equals(me)) {
-		    return getNodeA();
+			return getNodeB();
+		} else if (getNodeB().equals(me)) {
+			return getNodeA();
 		} else {
 			return null;
 		}
-    }
+	}
 
-    /**
-     * retrieve node A connected to this link
-     * @return the node connected 
-     */
-    private final IUserNode getNodeA() {
-    	return simulatorComponent.getNodeA();
-    }
+	/**
+	 * retrieve node A connected to this link
+	 * 
+	 * @return the node connected
+	 */
+	private final IUserNode getNodeA() {
+		return simulatorComponent.getNodeA();
+	}
 
-    /**
-     * retrieve node B connected to this link
-     * @return the node connected
-     */
-    private final IUserNode getNodeB() {
-    	return simulatorComponent.getNodeB();
-    }
+	/**
+	 * retrieve node B connected to this link
+	 * 
+	 * @return the node connected
+	 */
+	private final IUserNode getNodeB() {
+		return simulatorComponent.getNodeB();
+	}
 
-    public final long getDelay() {
-    	return simulatorComponent.getDelay();
-    }
+	public final long getDelay() {
+		return simulatorComponent.getDelay();
+	}
 
-    public String toString() {
-    	return "Link#" + getId();
-    }
-    
-    protected String getId() {
-    	return simulatorComponent.getId();
-    }
+	public String toString() {
+		return "Link#" + getId();
+	}
 
-    public final void interrupt() {
+	protected String getId() {
+		return simulatorComponent.getId();
+	}
+
+	public final void interrupt() {
 		try {
-		    simulatorComponent.interrupt();
+			simulatorComponent.interrupt();
 		} catch (NullPointerException e) {
-		    // nothing
+			// nothing
 		}
-    }
-    
-    public final void disconnect() {
-    	simulatorComponent.disconnect();
-    }
+	}
 
-    public final void sleep(int steps) {
+	public final void disconnect() {
+		simulatorComponent.disconnect();
+	}
+
+	public final void sleep(int steps) {
 		try {
-		    simulatorComponent.sleep(steps);
+			simulatorComponent.sleep(steps);
 		} catch (NullPointerException e) {
-		    // nothing
+			// nothing
 		}
-    }
-    
-    public final List<IUserPacket> getPacketsOnLink() {
-    	return simulatorComponent.getPacketsOnLink();
-    }
-    
-    public final void dropPacketOnLink(IUserPacket packet) {
-    	simulatorComponent.dropPacketOnLink(packet);
-    }
-    
-    public final void dropPacketsOnLink() {
-    	simulatorComponent.dropPacketsOnLink();
-    }
+	}
 
-    public final AVariable getVariable(String identifier) {
-    	return simulatorComponent.getScopedVariable(COMMON_SCOPES.USER, identifier);
-    }
+	public final List<IUserPacket> getPacketsOnLink() {
+		return simulatorComponent.getPacketsOnLink();
+	}
 
-    public final boolean hasVariable(String identifier) {
-    	return simulatorComponent.hasScopedVariable(COMMON_SCOPES.USER, identifier);
-    }
+	public final void dropPacketOnLink(IUserPacket packet) {
+		simulatorComponent.dropPacketOnLink(packet);
+	}
+
+	public final void dropPacketsOnLink() {
+		simulatorComponent.dropPacketsOnLink();
+	}
+
+	public final AVariable getVariable(String identifier) {
+		return simulatorComponent.getScopedVariable(COMMON_SCOPES.USER,
+				identifier);
+	}
+
+	public final boolean hasVariable(String identifier) {
+		return simulatorComponent.hasScopedVariable(COMMON_SCOPES.USER,
+				identifier);
+	}
 }
