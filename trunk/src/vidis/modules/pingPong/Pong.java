438f544ae023582c6838a2d2396bb306447f68cd
admin@lab-9.com <admin@lab-9.com@9cfe6e55-b949-0410-9339-5d950d6e6b25>
2009-02-14 17:30:02 +0000

    ping pong demo for presentation

---
+++ trunk/src/vidis/modules/pingPong/Pong.java
@@ -0,0 +1,11 @@
+package vidis.modules.pingPong;
+
+import vidis.data.AUserPacket;
+import vidis.data.annotation.ColorType;
+import vidis.data.annotation.ComponentColor;
+import vidis.data.annotation.ComponentInfo;
+
+@ComponentInfo(name="Pong")
+@ComponentColor(color=ColorType.YELLOW)
+public class Pong extends AUserPacket {
+}
