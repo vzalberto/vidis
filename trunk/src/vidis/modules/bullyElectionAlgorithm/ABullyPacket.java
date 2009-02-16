aabbf26144d0edc41808b433cfbe05f36b530fe2
christoph.caks <christoph.caks@9cfe6e55-b949-0410-9339-5d950d6e6b25>
2009-02-16 19:51:02 +0000

    refactored bully election v2 to bully election

--- trunk/src/vidis/modules/bullyElectionAlgorithm/ABullyPacket.java
+++ trunk/src/vidis/modules/bullyElectionAlgorithm/ABullyPacket.java
@@ -3,35 +3,25 @@
 	This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
 	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 	You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>. */
-package vidis.modules.bullyElectionAlgorithm_v2;
+package vidis.modules.bullyElectionAlgorithm;
 
-import vidis.data.annotation.ColorType;
-import vidis.data.annotation.ComponentColor;
-import vidis.data.annotation.Display;
-import vidis.data.annotation.DisplayColor;
+import vidis.data.AUserPacket;
 
-@ComponentColor(color=ColorType.GREEN)
-public class ElectionPacket extends ABullyPacket {
-
-    private String id;
-
-    public ElectionPacket(String id) {
-    	this.id = id;
-    }
-
-    public String getBullyId() {
-    	return id;
-    }
-    
-    @DisplayColor
-    public ColorType color = ColorType.GREEN;
-    
-    @Display ( name="name" )
-    public String toString() {
-    	return "Elect{"+getBullyId()+","+getHops()+"/"+getMaxHops()+"}";
-    }
-
-	@Override
+public abstract class ABullyPacket extends AUserPacket {
+	protected int hopsLeft;
+	public ABullyPacket() {
+		setHops(getMaxHops());
+	}
+	public ABullyPacket(int hops) {
+		super();
+		setHops(hops);
+	}
+	public void setHops(int hops) {
+		hopsLeft = hops;
+	}
+	public int getHops() {
+		return hopsLeft;
+	}
 	public int getMaxHops() {
 		return 15;
 	}
