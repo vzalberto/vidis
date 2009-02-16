52fb624df16d7536e6619463a5a069e053d31aac
christoph.caks <christoph.caks@9cfe6e55-b949-0410-9339-5d950d6e6b25>
2009-02-16 19:51:02 +0000

    refactored bully election v2 to bully election

--- src/vidis/modules/bullyElectionAlgorithm/ElectionPacket.java
+++ src/vidis/modules/bullyElectionAlgorithm/ElectionPacket.java
@@ -3,37 +3,36 @@
 	This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
 	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 	You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>. */
-package vidis.modules.bullyElectionAlgorithm_v2;
+package vidis.modules.bullyElectionAlgorithm;
 
 import vidis.data.annotation.ColorType;
 import vidis.data.annotation.ComponentColor;
 import vidis.data.annotation.Display;
 import vidis.data.annotation.DisplayColor;
 
-@ComponentColor(color=ColorType.CYAN)
-public class PongPacket extends ABullyPacket {
+@ComponentColor(color=ColorType.GREEN)
+public class ElectionPacket extends ABullyPacket {
 
     private String id;
-    private String senderId;
-    
-    @DisplayColor
-    public ColorType color = ColorType.YELLOW;
 
-    public PongPacket(String id, String senderId) {
+    public ElectionPacket(String id) {
     	this.id = id;
-    	this.senderId = senderId;
     }
 
     public String getBullyId() {
     	return id;
     }
     
-    public String getSenderId() {
-    	return senderId;
-    }
+    @DisplayColor
+    public ColorType color = ColorType.GREEN;
     
     @Display ( name="name" )
     public String toString() {
-    	return "Pong{"+getBullyId()+","+getSenderId()+","+getHops()+"/"+getMaxHops()+"}";
+    	return "Elect{"+getBullyId()+","+getHops()+"/"+getMaxHops()+"}";
     }
+
+	@Override
+	public int getMaxHops() {
+		return 15;
+	}
 }
--- src/vidis/modules/bullyElectionAlgorithm/ElectionRestartPacket.java
+++ src/vidis/modules/bullyElectionAlgorithm/ElectionRestartPacket.java
@@ -3,25 +3,35 @@
 	This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; either version 3 of the License, or (at your option) any later version.
 	This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 	You should have received a copy of the GNU General Public License along with this program; if not, see <http://www.gnu.org/licenses/>. */
-package vidis.modules.bullyElectionAlgorithm_v2;
+package vidis.modules.bullyElectionAlgorithm;
 
-import vidis.data.AUserPacket;
+import vidis.data.annotation.ColorType;
+import vidis.data.annotation.ComponentColor;
+import vidis.data.annotation.Display;
+import vidis.data.annotation.DisplayColor;
 
-public abstract class ABullyPacket extends AUserPacket {
-	protected int hopsLeft;
-	public ABullyPacket() {
-		setHops(getMaxHops());
-	}
-	public ABullyPacket(int hops) {
-		super();
-		setHops(hops);
-	}
-	public void setHops(int hops) {
-		hopsLeft = hops;
-	}
-	public int getHops() {
-		return hopsLeft;
-	}
+@ComponentColor(color=ColorType.RED)
+public class ElectionRestartPacket extends ABullyPacket {
+
+    private String id;
+    
+    public ElectionRestartPacket(String id) {
+    	this.id = id;
+    }
+
+    public String getBullyId() {
+    	return id;
+    }
+    
+    @DisplayColor
+    public ColorType color = ColorType.BLUE;
+    
+    @Display ( name="name" )
+    public String toString() {
+    	return "Restart{"+getBullyId()+","+getHops()+"/"+getMaxHops()+"}";
+    }
+
+	@Override
 	public int getMaxHops() {
 		return 15;
 	}
